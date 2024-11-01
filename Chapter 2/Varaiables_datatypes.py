b9faa517520d700fa8a4024e5e8d58aed76f4207
VIPUL <63034965+vipulsamantgithub@users.noreply.github.com>
2024-11-01 11:23:21 +0530

    Add files via upload

---
+++ Chapter 2/Varaiables_datatypes.py
@@ -0,0 +1,16 @@
+# Variables is the name given to the memory location and is used to store value.
+
+"""
+a='2.5'
+print(type(a))
+
+b=None
+print(type(b))
+
+c=False
+print(type(c))
+"""
+
+# 'alt +shift + Down arrow button' - to replicate the line
+
+
